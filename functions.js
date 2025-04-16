340bad2fe80d45bda508900629fdac572331d60b
e-Rat <kevin.debrownies@interia.pl>
2025-04-16 13:00:39 +0200

    Funkcja SUB i DIV

--- functions.js
+++ functions.js
@@ -6,20 +6,33 @@ var Functions = {
         console.log("STORE");
         var akumulatorRow = document.getElementById("memoryRow0");
         var akumulatorValue = akumulatorRow.cells[1].querySelector("input").value;
-
-        var programRow = document.getElementById(programCurrentRow);
-        var argument = programRow.cells[4].querySelector("input").value;
-
-        var memoryRow = document.getElementById("memoryRow" + argument);
-        memoryRow.cells[1].querySelector("input").value = akumulatorValue;
+        if(!akumulatorValue) {
+            alert("Wartosc musi byc ustalona");
+            return;
+        }
+        else{
+            var programRow = document.getElementById(programCurrentRow);
+            var instrucion = programRow.cells[3].querySelector("select").value;
+            var argument = programRow.cells[4].querySelector("input").value;
+    
+            document.getElementById("Instruction").value = instrucion;
+            document.getElementById("Argument").value = argument;
+    
+            var memoryRow = document.getElementById("memoryRow" + argument);
+            memoryRow.cells[1].querySelector("input").value = akumulatorValue;
+        }
     },
     READ: function() { //Bartek
 
     },
     WRITE: function() { //Eryk
         var programRow = document.getElementById(programCurrentRow);
+        var instrucion = programRow.cells[3].querySelector("select").value;
         var argument = programRow.cells[4].querySelector("input").value;
         console.log(argument);
+
+        document.getElementById("Instruction").value = instrucion;
+        document.getElementById("Argument").value = argument;
 
         var memoryRow = document.getElementById("memoryRow" + argument);
         var memoryValue = memoryRow.cells[1].querySelector("input").value;
@@ -32,12 +45,38 @@ var Functions = {
     },
     SUB: function() { //Eryk
         console.log("SUB");
+        var programRow = document.getElementById(programCurrentRow);
+        var instrucion = programRow.cells[3].querySelector("select").value;
+        var argument = programRow.cells[4].querySelector("input").value;
+        console.log(argument);
+
+        document.getElementById("Instruction").value = instrucion;
+        document.getElementById("Argument").value = argument;
+
+        var memoryRow = document.getElementById("memoryRow" + argument);
+        var memoryValue = memoryRow.cells[1].querySelector("input").value;
+
+        var akumulatorRow = document.getElementById("memoryRow0");
+        akumulatorRow.cells[1].querySelector("input").value -= memoryValue;
     },
     MULT: function() { //Bartek
         console.log("MULT");
     },
     DIV: function() { //Eryk
         console.log("DIV");
+        var programRow = document.getElementById(programCurrentRow);
+        var instrucion = programRow.cells[3].querySelector("select").value;
+        var argument = programRow.cells[4].querySelector("input").value;
+        console.log(argument);
+
+        document.getElementById("Instruction").value = instrucion;
+        document.getElementById("Argument").value = argument;
+
+        var memoryRow = document.getElementById("memoryRow" + argument);
+        var memoryValue = memoryRow.cells[1].querySelector("input").value;
+
+        var akumulatorRow = document.getElementById("memoryRow0");
+        akumulatorRow.cells[1].querySelector("input").value /= memoryValue;
     },
     JUMP: function() { //Bartek
         console.log("JUMP");
